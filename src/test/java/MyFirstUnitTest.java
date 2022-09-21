c6d0a182a35ec6af0c7a6da342407238c2ac0cbb
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-09-21 19:34:47 +0200

    my first test

--- src/test/java/MyFirstUnitTest.java
+++ src/test/java/MyFirstUnitTest.java
@@ -1,3 +1,19 @@
-public class MyFirstUnitTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyFirstUnitTest { // klasa testowa
+
+    @Test // adnotacja oznaczająca, że motdoa jest testem
+    void shouldMultiplyTwoNumbers() { //sygnatura metody testowej
+        //given //warunki początkowe
+        final double firstNumber = 2;
+        final double secondNumber = 3;
+
+        //when //wykonanie testu
+        final double multiplicationResult = firstNumber * secondNumber;
+
+        //then // sprawdzenie testu i wynik
+        assertEquals(6, multiplicationResult);
+    }
 }
