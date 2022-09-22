0dfa112bfeadfdc4e78ca9377b2c2693857ee864
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-09-22 20:27:47 +0200

    tdd example - fibonacci

---
+++ src/test/java/tdd/FibonacciTest.java
@@ -0,0 +1,26 @@
+package tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FibonacciTest {
+
+    @Test
+    void shouldReturnZeroForZeroElement() {
+        assertEquals(0, Fibonacci.getValue(0));
+    }
+
+    @Test
+    void shouldReturnOneForFirstElement() {
+        assertEquals(1, Fibonacci.getValue(1));
+    }
+
+    @Test
+    void shouldReturnValue() {
+        assertEquals(8, Fibonacci.getValue(6));
+        assertEquals(144, Fibonacci.getValue(12));
+        assertEquals(987, Fibonacci.getValue(16));
+
+    }
+}
