af2cf7460d049de096c4172007f4311360ba367c
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-09-22 18:17:52 +0200

    Calculator test example

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    @Test
     void shouldAddTwoNumber() {
         final int valueA = 3;
         final int valueB = 5;
