277a1031cbe040c52833bdd5af1bbd360365de08
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-09-22 18:31:30 +0200

    Calculator test example refactor

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,19 +1,37 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calculator; // deklaracja zmiennej
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator(); // inizjalizacja zmiennej
+    }
+
+
+
     @Test
     void shouldAddTwoNumber() {
         final int valueA = 3;
         final int valueB = 5;
 
-        Calculator calculator = new Calculator();
-
         final int result = calculator.add(valueA, valueB);
 
         assertEquals(8, result);
+    }
+
+    @Test
+    void shouldMultiplyTwoNumber() {
+        final int valueA = 3;
+        final int valueB = 5;
+
+        final int result = calculator.multiply(valueA, valueB);
+
+        assertEquals(15, result);
     }
 
 
